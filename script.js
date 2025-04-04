682a2763ef70bda02d2e60036f60f50de824a9f7
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-04-04 21:21:06 +0200

    ...

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const playerScore1 = document.querySelector(".player1-score");
+const playerScore2 = document.querySelector(".player2-score");
+const currentScore1 = document.querySelector(".current-score1");
+const currentScore2 = document.querySelector(".current-score2");
+const randomNumber = document.querySelector(".random-number");
+const newGameBtn = document.querySelector(".new-game");
+const generateBtn = document.querySelector(".generate-number");
+const holdBtn = document.querySelector(".hold");
